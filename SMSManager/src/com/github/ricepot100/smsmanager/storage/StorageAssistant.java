4edb14d6017f8d9c500b87ac927c66a6ab7e68bc
ricepot100 <ricepot100@163.com>
2014-10-31 23:27:04 +0800

    Add storage information

--- SMSManager/src/com/github/ricepot100/smsmanager/storage/StorageAssistant.java
+++ SMSManager/src/com/github/ricepot100/smsmanager/storage/StorageAssistant.java
@@ -1,5 +1,46 @@
 package com.github.ricepot100.smsmanager.storage;
 
-public class StorageAssistant {
+import java.io.File;
 
+import com.github.ricepot100.smsmanager.Assistant;
+
+import android.os.Environment;
+import android.util.Log;
+
+public class StorageAssistant {
+	private static String s_ExtStorageAbsoluteDirectory = null;
+	private static String s_RootStorageAbsoluteDirectory = null;
+	public static boolean createInitStorage() {
+		if (Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+			File f_extStorage = Environment.getExternalStorageDirectory();
+			s_ExtStorageAbsoluteDirectory = f_extStorage.getAbsolutePath();
+			
+			File f_RootStorage = new File(s_ExtStorageAbsoluteDirectory + "/" +
+					Assistant.RootDirectory);
+			if (!f_RootStorage.exists()) {
+				if (true == f_RootStorage.mkdirs()) {
+					s_RootStorageAbsoluteDirectory = f_RootStorage.getAbsolutePath();
+				} else {
+					Log.d(Assistant.TAG, "Can not create direcotry for " + f_RootStorage.getAbsolutePath());
+					return false;
+				}
+			} else if (f_RootStorage.isDirectory()) {
+				s_RootStorageAbsoluteDirectory = f_RootStorage.getAbsolutePath();
+			} else {
+				Log.d(Assistant.TAG, "The file " + f_RootStorage.getAbsolutePath() + " exist, and not a directory");
+				return false;
+			}
+		} else {
+			Log.d(Assistant.TAG, "Have no external storage media");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static void WriteToRecord(String text) {
+		if (StorageAssistant.createInitStorage()) {
+			
+		}
+	}
 }
