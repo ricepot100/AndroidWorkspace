b9d05a60414f29d8b0f5777d9b9d5ebf427942d1
ricepot100 <ricepot100@163.com>
2014-11-01 14:52:40 +0800

    calling status debug

---
+++ CallManager/src/com/github/ricepot100/callmanager/Assistant.java
@@ -0,0 +1,6 @@
+package com.github.ricepot100.callmanager;
+
+public interface Assistant {
+	public static final String TAG = "ricepot100";
+	public static final String RootDirectory = "CallManager";		// /sdcard/SMSManage
+}
---
+++ CallManager/src/com/github/ricepot100/callmanager/CallingReceivBroadcast.java
@@ -0,0 +1,16 @@
+package com.github.ricepot100.callmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class CallingReceivBroadcast extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		Log.d(Assistant.TAG, "onReceiver: action: " + intent.getAction());
+	}
+
+}
