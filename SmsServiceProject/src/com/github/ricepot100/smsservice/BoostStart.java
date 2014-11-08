213c5c4788a031806b924eb2bc6297022b74dd78
ricepot100 <ricepot100@163.com>
2014-11-08 22:33:14 +0800

    Add sms database observer

---
+++ SmsServiceProject/src/com/github/ricepot100/smsservice/BoostStart.java
@@ -0,0 +1,18 @@
+package com.github.ricepot100.smsservice;
+
+import com.github.ricepot100.smsservice.smsdatabase.SMSDBService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BoostStart extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		Intent s_intent = new Intent(context, SMSDBService.class);
+		context.getApplicationContext().startService(intent);
+	}
+
+}
--- SmsServiceProject/src/com/github/ricepot100/smsservice/smsdatabase/SMSDBService.java
+++ SmsServiceProject/src/com/github/ricepot100/smsservice/smsdatabase/SMSDBService.java
@@ -2,9 +2,14 @@ package com.github.ricepot100.smsservice.smsdatabase;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 
 public class SMSDBService extends Service {
+	
+	private SMSDBInBoxContentObserver m_SmsDBInBoxObserver = null;
+	private SMSDBSendBoxContentObserver m_SmsDBSendBoxObserver = null;
+	private Handler m_handler = null;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -14,12 +19,28 @@ public class SMSDBService extends Service {
 	
 	@Override
 	public void onCreate() {
-		
+		m_handler = new Handler();
+		m_SmsDBInBoxObserver = new SMSDBInBoxContentObserver(this, m_handler);
+		m_SmsDBInBoxObserver.registerToContext();
+		m_SmsDBSendBoxObserver = new SMSDBSendBoxContentObserver(this, m_handler);
+		m_SmsDBSendBoxObserver.registerToContext();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId){
 		super.onStartCommand(intent, flags, startId);
 		return START_STICKY;
+	}
+	
+	@Override
+	public void onDestroy() {
+		if(null != m_SmsDBInBoxObserver) {
+			m_SmsDBInBoxObserver.unregisterFromContext();
+			m_SmsDBInBoxObserver=null;
+		}
+		if (null != m_SmsDBSendBoxObserver) {
+			m_SmsDBSendBoxObserver.unregisterFromContext();
+			m_SmsDBSendBoxObserver = null;
+		}
 	}
 }
